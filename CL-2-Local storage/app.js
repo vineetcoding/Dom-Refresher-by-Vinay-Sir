1dfef8c62ec233a55ee524b5a9de67022f74afb1
vineetcoding <114997519+vineetcoding@users.noreply.github.com>
2023-01-01 01:45:20 +0530

    2nd class

--- CL-2-Local storage/app.js
+++ CL-2-Local storage/app.js
@@ -6,8 +6,8 @@ const list = document.querySelector('.list');
 let arr = [];
 btn.addEventListener('click', (e)=>{
     e.preventDefault();
-    console.log(input.value);
-    list.innerHTML +=  `<li>${input.value}<button id="delete">Delete</button></li>`
+    // console.log(input.value);
+    list.innerHTML +=  `<li>${input.value}</li><button class="delete">Delete</button>`
 
     arr.push(input.value);
     localStorage.setItem("input", JSON.stringify(arr));
@@ -16,8 +16,29 @@ btn.addEventListener('click', (e)=>{
 
 window.addEventListener("load", (()=>{
     arr = JSON.parse(localStorage.getItem("input")) || []
-    console.log(arr);
+    // console.log(arr);
     arr.forEach(element => {
-       list.innerHTML += `<li>${element}<button id="delete">Delete</button></li>` 
+       list.innerHTML += `<li>${element}</li><button class="delete">Delete</button>` 
     });
 }))
+
+const delbtn = document.querySelector(".list")
+
+delbtn.addEventListener('click', (e)=>{
+    // const del = document.querySelector('.delete');
+    // console.log(del);
+    console.log(e.target && e.target.previousSibling);
+    let deltext = e.target.previousSibling.innerText;
+    let index = arr.indexOf(deltext);
+    arr.splice(index, 1);
+    // console.log(arr);
+    localStorage.setItem("input", JSON.stringify(arr))
+    // console.log();
+    // e.target.parentNode.remove()
+    // e.target.previousSibling.remove()
+
+    // list.innerHTML = "";
+    arr = JSON.parse(localStorage.getItem('input'))
+
+
+})
